c2fea6f3fb03b07aa5a3e9135035f34388ac23de
Valentin Dao <valentin.dao3908@gmail.com>
2025-09-13 12:37:17 +0200

    release v2.0.1

--- build.lua
+++ build.lua
@@ -13,3 +13,21 @@ typesetexe = "lualatex"
 typesetopts = "-interaction=nonstopmode -shell-escape"
 
 distribdir = "public"
+
+uploadconfig = {
+  pkg           = "intexgral",
+  version       = "v2.0.1 2025-09-13",
+  author        = "Valentin Dao",
+  uploader      = "Valentin Dao",
+  email         = "vdao.texdev@gmail.com",
+  license       = "lppl1.3c",
+  summary       = "A LaTeX package for typesetting integrals",
+  ctanPath      = "/macros/latex/contrib/intexgral",
+  announcement  = [[
+      Fixed: 
+      - Compatibility issue between amssymb and unicode-math.
+  ]],
+  update        = true,
+  repository    = "https://github.com/ankaa3908/intexgral/tree/main",
+  bugtracker    = "https://github.com/ankaa3908/intexgral/issues"
+}
