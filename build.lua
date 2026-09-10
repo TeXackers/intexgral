400f7bcb191690e6c85d07a7bc3952b136a458e9
Valentin Dao <valentin.dao3908@gmail.com>
2026-09-10 18:19:47 +0100

    release v4.2.0

--- build.lua
+++ build.lua
@@ -19,7 +19,7 @@ Typesetting integrals, although common in LaTeX, is not particularly practical. 
 
 uploadconfig = {
    pkg                 = "intexgral",
-   version             = "v4.1.1 2026-08-01",
+   version             = "v4.2.0 2026-09-10",
    author              = "Valentin Dao",
    uploader            = "Valentin Dao",
    email               = "vdao.texdev@gmail.com",
